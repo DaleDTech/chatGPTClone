cc5f8319189af2a936684870afee0d59972f41dc
dale das <daledas@dales-MacBook-Air.local>
2023-08-21 19:08:39 -0400

    basic ChatGPT clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
+
+  const getMessages = async () => {
+    const API_KEY = process.env.REACT_APP_SECRET_KEY;
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "Explain compound interest to a first time saver" }],
+        max_tokens: 2000,
+      })
+
+    }
+
+    try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+      const data = await response.json()
+      console.log("Content Answer:")
+      console.log(data.choices[0].message.content)
+    }
+
+    catch (error){
+      console.error(error)
+
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li> BLUGH </li>
+        </ul>
+      </section>
+      <nav>
+        <p> Made by Dale </p>
+      </nav>
+      <section className="main">
+        <h1>Dale's GPT</h1>
+        <ul className="feed">
+          <div className="bottom-section">
+            <div className="input-container">
+              <input />
+              <div id="submit" onClick={getMessages}>
+                SUBMIT
+              </div>
+            </div>
+
+            <p className="info">
+              Free Research Preview. ChatGPT may produce inaccurate information
+              about people, places, or facts. ChatGPT August 3 Version
+            </p>
+          </div>
+        </ul>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
